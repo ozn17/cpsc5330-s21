7203d8b57d3e1ac60c0496de425b8e9d39d5f920
hanks-su <42451381+hanks-su@users.noreply.github.com>
2021-04-09 16:27:26 -0700

    Restore docker

--- Docker/bda_class_image/conf/docker-entrypoint.sh
+++ Docker/bda_class_image/conf/docker-entrypoint.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 /etc/init.d/ssh start
+/etc/init.d/mysql start
 hdfs namenode -format
 $HADOOP_HOME/sbin/start-dfs.sh
 
-# Hive directories
-hdfs dfs -mkdir /tmp
-hdfs dfs -chmod g+w /tmp
-hdfs dfs -mkdir /user
-hdfs dfs -mkdir /user/hive
-hdfs dfs -mkdir /user/hive/warehouse
-hdfs dfs -chmod g+w /user/hive/warehouse
+# Disgusting!  To run sqoop with --bindir /tmp and Hadoop will find the generated jar.
+export HADOOP_CLASSPATH=`(hadoop classpath)`:/tmp
 
-/opt/hive/bin/schematool -initSchema -dbType derby
-
+echo 'create database world;' | mysql --user root
+mysql world --user root < /tmp/world.sql
+echo "create user training identified by 'training';" | mysql --user root
+echo "grant all privileges on *.* to training;" | mysql --user root
 exec "$@"
