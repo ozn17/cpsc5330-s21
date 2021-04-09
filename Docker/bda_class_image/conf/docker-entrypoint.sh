5922804b9b937950e2c86ba9270788334ded54d2
hanks-su <42451381+hanks-su@users.noreply.github.com>
2021-04-09 14:23:52 -0700

    Update docker

--- Docker/bda_class_image/conf/docker-entrypoint.sh
+++ Docker/bda_class_image/conf/docker-entrypoint.sh
@@ -4,4 +4,14 @@
 hdfs namenode -format
 $HADOOP_HOME/sbin/start-dfs.sh
 
+# Hive directories
+hdfs dfs -mkdir /tmp
+hdfs dfs -chmod g+w /tmp
+hdfs dfs -mkdir /user
+hdfs dfs -mkdir /user/hive
+hdfs dfs -mkdir /user/hive/warehouse
+hdfs dfs -chmod g+w /user/hive/warehouse
+
+/opt/hive/bin/schematool -initSchema -dbType derby
+
 exec "$@"
